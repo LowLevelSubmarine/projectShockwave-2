4a95797bb434a545dd8434177c6e1c80204ecae3
LowLevelSubmarine <33373745+LowLevelSubmarine@users.noreply.github.com>
2018-07-11 15:51:11 +0200

    0.13.2 ALPHA
    -Made artifactId inside of pom.xml valid

--- src/main/java/core/VersionInfo.java
+++ src/main/java/core/VersionInfo.java
@@ -2,7 +2,7 @@ package core;
 
 public class VersionInfo {
     public static final String PROJECTTITLE = "ProjectShockwave-2";
-    public static final String NAME = "0.13.1 ALPHA";
+    public static final String NAME = "0.13.2 ALPHA";
     public static final String TITLE = "Alpha Phase";
     public static final String CHANGELOG = "- availabel in alpha phase-";
 }
